53830eac112b22f325041b645341f24742bd7f65
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2024-10-10 17:11:05 -0300

    feat: create the signupForm component for user registration and add css styles

--- client/src/components/SignupForm/SignupForm.jsx
+++ client/src/components/SignupForm/SignupForm.jsx
@@ -1,25 +1,27 @@
 'use client';
 
+import Image from 'next/image';
 import { useEffect, useTransition } from 'react';
 import { useFormState } from 'react-dom';
 import { signup } from './action';
+import style from '../../styles/signup.module.css';
 
 // **Este componente solo es ejemplo**
 
 /**
- Como se ve al principio del archivo esta el string "use client"
+ Como se ve al principio del archivo esta el string 'use client'
  Esto se le llama directiva, y sirve para indicar que este componente se ejecutara del lado del cliente
  Se necesita ejecutar del lado del cliente debido a que utiliza eventos del navegador
  No disponibles en el cliente
 
- En este caso se utilizan los hooks "useEffect", "useTransition", "useFormState", solo disponibles en el cliente y ademas tambien el evento "onSubmit" solo disponible en el cliente(Todos los eventos de js solo estan disponibles en el cliente, APIS como window o document tambien)
+ En este caso se utilizan los hooks 'useEffect', 'useTransition', 'useFormState', solo disponibles en el cliente y ademas tambien el evento 'onSubmit' solo disponible en el cliente(Todos los eventos de js solo estan disponibles en el cliente, APIS como window o document tambien)
  */
 export const SignupForm = () => {
-  // Este hook de vuelva un "wrapper" del action
-  // Basicamente lo que hace es devolver una nueva funcion que llamo "dispatch"
+  // Este hook de vuelva un 'wrapper' del action
+  // Basicamente lo que hace es devolver una nueva funcion que llamo 'dispatch'
   // que se ejecuta en el cliente recibe los datos del formulario
-  // y ejecuta la funcion "signup" del lado del servidor
-  // El segundo parametro "result" es la respuesta(return) de la funcion "signup"
+  // y ejecuta la funcion 'signup' del lado del servidor
+  // El segundo parametro 'result' es la respuesta(return) de la funcion 'signup'
   const [result, dispatch] = useFormState(signup, undefined);
   const [isPending, startTransition] = useTransition();
 
@@ -36,36 +38,59 @@ export const SignupForm = () => {
     const formData = new FormData(event.currentTarget);
     // start transition es una funcion que se utiliza, para indicar una transicion
     // desde que se inicia a ejecutar hasta que se termine de ejecutar
-    // En este caso la transicion es mientras se ejecuta "dispatch"
+    // En este caso la transicion es mientras se ejecuta 'dispatch"
     startTransition(() => dispatch(formData));
   };
 
-  // Formulario de ejemplo
   return (
-    <form className="border-1 mx-auto flex max-w-2xl flex-col rounded-sm" onSubmit={handleSubmit}>
-      <div className="flex flex-col gap-y-4">
-        <div className="flex flex-col gap-y-2">
-          <label htmlFor="emailField">
-            Email
-            <input id="emailField" type="email" name="emailField" />
-          </label>
-        </div>
-        <div className="flex flex-col gap-y-2">
-          <label htmlFor="nameField">
-            Name
-            <input id="nameField" type="text" name="nameField" />
-          </label>
-        </div>
-        <div className="flex flex-col gap-y-2">
-          <label htmlFor="passwordField">
-            Email
-            <input id="passwordField" type="password" name="passwordField" />
-          </label>
-        </div>
+    <div className={style.signup}>
+      <div className={style.signupContentLogo}>
+        <Image
+          className={style.signupLogo}
+          src="/images/noCountryLogo.png"
+          alt="No Country logo"
+          width={56}
+          height={46}
+        />
+        <Image src="/images/noCountry.png" alt="No Country" width={66} height={16} />
       </div>
-      <button type="submit" disabled={isPending}>
-        Submit
-      </button>
-    </form>
+      <form className={style.signupFormContent} onSubmit={handleSubmit}>
+        <fieldset className={style.signupFieldSet}>
+          <div className={style.signupContent}>
+            <div className={style.signupContentLabel}>
+              <label className={style.signupLabel} htmlFor="userField">
+                Usuario
+                <input className={style.signupInput} id="user" type="text" name="userField" />
+              </label>
+            </div>
+            <div className={style.signupContentLabel}>
+              <label className={style.signupLabel} htmlFor="passwordField">
+                Contraseña
+                <input
+                  className={style.signupInput}
+                  id="password"
+                  type="password"
+                  name="passwordField"
+                />
+              </label>
+            </div>
+            <div className={style.signupContentLabel}>
+              <label className={style.signupLabel} htmlFor="repeatPasswordField">
+                Repetir contraseña
+                <input
+                  className={style.signupInput}
+                  id="repeat"
+                  type="password"
+                  name="repeatPasswordField"
+                />
+              </label>
+            </div>
+          </div>
+          <button className={style.signupButton} type="submit" disabled={isPending}>
+            Continuar
+          </button>
+        </fieldset>
+      </form>
+    </div>
   );
 };
