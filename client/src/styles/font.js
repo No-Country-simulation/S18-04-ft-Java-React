d0fccb4e77db531aec7f25cce03081a770fb1459
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2024-10-12 01:23:06 -0300

    refactor: remove open and put dm in the variable

--- client/src/styles/font.js
+++ client/src/styles/font.js
@@ -3,7 +3,7 @@ import { DM_Sans } from 'next/font/google';
 // Next descargara la fuenta de google y la optimizara automaticamente por ti
 export const dmSans = DM_Sans({
   subsets: ['latin'],
-  variable: '--font-open-sans',
+  variable: '--font-dm-sans',
   style: ['normal'],
   weight: ['400', '500', '600', '700'],
 });
