dfebfeef3d2d774f73cb0721e19c92a78f390320
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2024-10-12 00:59:04 -0300

    refactor: use reusable components and remove unused logic and comments

--- client/src/components/SignupForm/SignupForm.jsx
+++ client/src/components/SignupForm/SignupForm.jsx
@@ -7,109 +7,80 @@ import { useState } from 'react';
 import style from '../../styles/signup.module.css';
 import Button from '../Button/Button';
 import { Divider } from '../Divider/Divider';
+import Form from '../Form/Form';
+import Input from '../Input/Input';
 
-/**
- Como se ve al principio del archivo esta el string 'use client'
- Esto se le llama directiva, y sirve para indicar que este componente se ejecutara del lado del cliente
- Se necesita ejecutar del lado del cliente debido a que utiliza eventos del navegador
- No disponibles en el cliente
-
- En este caso se utilizan los hooks 'useEffect', 'useTransition', 'useFormState', solo disponibles en el cliente y ademas tambien el evento 'onSubmit' solo disponible en el cliente(Todos los eventos de js solo estan disponibles en el cliente, APIS como window o document tambien)
- */
 export const SignupForm = () => {
-  // Este hook de vuelva un 'wrapper' del action
-  // Basicamente lo que hace es devolver una nueva funcion que llamo 'dispatch'
-  // que se ejecuta en el cliente recibe los datos del formulario
-  // y ejecuta la funcion 'signup' del lado del servidor
-  // El segundo parametro 'result' es la respuesta(return) de la funcion 'signup'
-  // const [result, dispatch] = useFormState(signup, undefined);
-  // const [isPending, startTransition] = useTransition();
+  const [passwordVisibility, setPasswordVisibility] = useState({
+    passwordOne: false,
+    passwordtwo: false,
+  });
 
-  // useEffect(() => {
-  //   if (result) {
-  //     // De esta forma se asegura el RESET de isPending
-  //     startTransition(() => {});
-  //   }
-  // }, [result]);
+  const [formValues, setFormValues] = useState({ passwordOne: '', passwordTwo: '' });
 
-  // const handleSubmit = event => {
-  //   //Prevent default para evitar que recargue la pagina (accion por defecto)
-  //   event.preventDefault();
-  //   const formData = new FormData(event.currentTarget);
-  //   // start transition es una funcion que se utiliza, para indicar una transicion
-  //   // desde que se inicia a ejecutar hasta que se termine de ejecutar
-  //   // En este caso la transicion es mientras se ejecuta 'dispatch"
-  //   startTransition(() => dispatch(formData));
-  // };
-
-  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
+  const togglePasswordVisibility = field => {
+    setPasswordVisibility(prevState => ({
+      ...prevState,
+      [field]: !prevState[field],
+    }));
+    console.log('hola aweonada');
   };
-
-  const IconSpan = (iconSrc, extraClass = '', onClick) => (
-    <span className={[style.signupIconSpan, extraClass].join(' ')} onClick={onClick}>
-      <Image src={iconSrc} width={18} height={18} alt="iconos de contraseña" />
-    </span>
-  );
 
   const handleSubmit = evt => {
     evt.preventDefault();
     router.push('/signupSecond');
   };
 
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    setFormValues(prevValues => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className={style.signup}>
-      <form className={style.signupFormContent} onSubmit={handleSubmit}>
-        <fieldset className={style.signupFieldSet}>
-          <div className={style.signupContent}>
-            <div className={style.signupContentLabel}>
-              <label className={style.signupLabel} htmlFor="userField">
-                Usuario
-                <input className={style.signupInput} id="user" type="text" name="userField" />
-                <span className={style.signupIconSpan}>
-                  <Image src="/images/user.png" width={18} height={18} alt="icono usuario" />
-                </span>
-              </label>
-            </div>
+      <Form onSubmit={handleSubmit}>
+        <div className={style.signupContent}>
+          <Input
+            id="user"
+            iconLeft="/images/user.png"
+            label="Usuario"
+            type="email"
+            autoComplete="new-email"
+          />
+          <Input
+            id="passwordOne"
+            name="passwordOne"
+            iconLeft="/images/unlock.png"
+            iconRight={passwordVisibility.passwordOne ? '/images/eyeOn.png' : '/images/eyeOff.png'}
+            label="Contraseña"
+            type={passwordVisibility.passwordOne ? 'text' : 'password'}
+            onIconRightClick={() => togglePasswordVisibility('passwordOne')}
+            value={formValues.passwordOne}
+            onChange={handleChange}
+            autoComplete="new-password"
+          />
 
-            <div className={style.signupContentLabel}>
-              <label className={style.signupLabel} htmlFor="passwordField">
-                Contraseña
-                <input
-                  className={style.signupInput}
-                  id="password"
-                  type={showPassword ? 'text' : 'password'}
-                  name="passwordField"
-                />
-                {IconSpan('/images/unlock.png')}
-                {IconSpan('/images/eyeOff.png', style.signupIconSpanRight)}
-              </label>
-            </div>
+          <Input
+            id="passwordTwo"
+            name="passwordTwo"
+            iconLeft="/images/unlock.png"
+            iconRight={passwordVisibility.passwordTwo ? '/images/eyeOn.png' : '/images/eyeOff.png'}
+            label="Repetir Contraseña"
+            type={passwordVisibility.passwordTwo ? 'text' : 'password'}
+            onIconRightClick={() => togglePasswordVisibility('passwordTwo')}
+            value={formValues.passwordTwo}
+            onChange={handleChange}
+            autoComplete="new-password"
+          />
+        </div>
 
-            <div className={style.signupContentLabel}>
-              <label className={style.signupLabel} htmlFor="repeatPasswordField">
-                Repetir contraseña
-                <input
-                  className={style.signupInput}
-                  id="repeat"
-                  type="password"
-                  name="repeatPasswordField"
-                />
-                {IconSpan('/images/unlock.png')}
-                {IconSpan(
-                  showPassword ? '/images/eyeOn.png' : '/images/eyeOff.png',
-                  style.signupIconSpanRight,
-                  togglePasswordVisibility
-                )}
-              </label>
-            </div>
-          </div>
-          <Button name="Continuar" type="submit" />
-        </fieldset>
-      </form>
+        <Button name="Continuar" type="submit" />
+      </Form>
       <Divider />
     </div>
   );
