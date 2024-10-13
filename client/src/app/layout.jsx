f03fbb5a93b2aafdccc985df526045813b51a22b
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2024-10-12 22:28:31 -0300

    feat: import normalize.css styles

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import { dmSans } from '@/styles/font';
 import '../styles/globals.css';
+import 'normalize.css';
 
 export default function RootLayout({ children }) {
   return (
