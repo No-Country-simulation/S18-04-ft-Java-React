dea87a9afdf8b9c2c25e7b1b15ae2557d54d0404
Catriel Escobar <125727012+Catriel-Escobar@users.noreply.github.com>
2024-10-15 21:46:46 -0300

    add cors

--- noCountryback/src/main/java/com/nocountry/nocountry/security/SecurityConfig.java
+++ noCountryback/src/main/java/com/nocountry/nocountry/security/SecurityConfig.java
@@ -155,7 +155,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://s18-04-ft-java-react-production.up.railway.app", "*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000/", "https://no-country.up.railway.app","https://nocountry.up.railway.app", "*"));
         configuration.setAllowedMethods(
                 Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"));
         configuration.setAllowedHeaders(List.of("*"));
