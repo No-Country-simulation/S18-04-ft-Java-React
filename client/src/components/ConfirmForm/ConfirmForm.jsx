98542170d4eeb958271c7cdd26ca497345f244a4
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2024-10-17 04:18:48 -0300

    feat: create form logic for user registrations

--- client/src/components/ConfirmForm/ConfirmForm.jsx
+++ client/src/components/ConfirmForm/ConfirmForm.jsx
@@ -1,4 +1,5 @@
 'use client';
+import { updateProfile } from './action';
 import style from '../../styles/signup.module.css';
 import FormComponent from '../Form/FormComponent';
 import { registerStepTwo } from '@/constants/authFields';
@@ -6,12 +7,13 @@ import { registerStepTwo } from '@/constants/authFields';
 export const ConfirmForm = () => {
   return (
     <section className={style.signup}>
+      <h2 className={style.signupSubtitle}>Completa estos campos para personalizar tu perfil.</h2>
       <FormComponent
         fields={registerStepTwo}
         className="relative mb-7 w-full items-center justify-center max-sm:max-w-[85%]"
         btnClassName="mt-[4.25rem] max-sm:max-w-[85%] mx-auto justify-center items-center"
         btnText="Continuar"
-        onSubmit={async () => {}}
+        onSubmit={updateProfile}
       />
     </section>
   );
