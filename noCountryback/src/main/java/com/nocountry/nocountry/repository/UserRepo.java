24b7c2da1b5c12cb0d93a03a81c0a9165f530c68
Anderson Cusma <andersoncusma20@gmail.com>
2024-10-16 23:57:39 -0500

    user repo

--- noCountryback/src/main/java/com/nocountry/nocountry/repository/UserRepo.java
+++ noCountryback/src/main/java/com/nocountry/nocountry/repository/UserRepo.java
@@ -2,6 +2,7 @@ package com.nocountry.nocountry.repository;
 
 import com.nocountry.nocountry.models.User;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +10,5 @@ import java.util.UUID;
 @Repository
 public interface UserRepo extends GenericRepo<User,UUID> {
     @Query(value = "SELECT * FROM users u WHERE u.email = :email", nativeQuery = true)
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(@Param("email") String email);
 }
