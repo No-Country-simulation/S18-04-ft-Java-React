c1fea11eaf2be0bffb59f7403f1fd4663256c373
Catriel Escobar <125727012+Catriel-Escobar@users.noreply.github.com>
2024-10-19 15:31:03 -0300

    create y find perfil fixeados

--- noCountryback/src/main/java/com/nocountry/nocountry/repository/ProfileRepo.java
+++ noCountryback/src/main/java/com/nocountry/nocountry/repository/ProfileRepo.java
@@ -1,8 +1,13 @@
 package com.nocountry.nocountry.repository;
 
 import com.nocountry.nocountry.models.Profile;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProfileRepo extends GenericRepo<Profile, UUID> {
+
+	@Query(value = "SELECT p FROM Profile p WHERE p.user.id = ?1")
+	Optional<Profile> findProfileByUserId( UUID userId);
 }
