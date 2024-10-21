76645ffd32b5f3e6e159509e5e1c75132694dfaa
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-10-21 20:03:12 -0300

    feat: add check login

--- client/src/app/(authenticated)/home/page.jsx
+++ client/src/app/(authenticated)/home/page.jsx
@@ -1,3 +1,4 @@
+import { CheckLogin } from '@/components/CheckLogin/CheckLogin';
 import { HomeArticles } from '@/components/sections/HomeArticles/HomeArticles';
 import { HomeCountdownSection } from '@/components/sections/HomeCountdownSection/HomeCountdownSection';
 import { HomeMain } from '@/components/sections/HomeMain/HomeMain';
@@ -6,6 +7,7 @@ import { HomeWidget } from '@/components/sections/HomeWidget/HomeWidget';
 export default function HomePage() {
   return (
     <>
+      <CheckLogin />
       <HomeMain name="Marcos" />
       <HomeWidget />
       <HomeCountdownSection />
---
+++ client/src/components/CheckLogin/CheckLogin.jsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const payload = {
+  method: 'GET',
+  headers: { 'Content-Type': 'application/json' },
+};
+
+export const CheckLogin = () => {
+  const [val, setVal] = useState(false);
+  useEffect(() => {
+    console.log('CheckLogin');
+
+    if (val) {
+      try {
+        fetch('https://nocountry.up.railway.app/api/auth/check-login', payload).then(res => {
+          console.log({ res });
+        });
+      } catch (e) {
+        console.log({ e });
+      }
+      setVal(true);
+    }
+  }, []);
+  return <div className="sr-only">Check login</div>;
+};
