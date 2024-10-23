a259a001cad077089d36cd0b3a3fb69b39ea429c
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-10-22 23:37:53 -0300

    fix: tu vieja 5.0

--- client/src/app/(authenticated)/home/page.jsx
+++ client/src/app/(authenticated)/home/page.jsx
@@ -3,10 +3,11 @@ import { HomeCountdownSection } from '@/components/sections/HomeCountdownSection
 import { HomeMain } from '@/components/sections/HomeMain/HomeMain';
 import { HomeWidget } from '@/components/sections/HomeWidget/HomeWidget';
 
-export default function HomePage() {
+export default async function HomePage({ searchParams }) {
+  const token = (await searchParams)?.token;
   return (
     <>
-      <HomeMain name="Marcos" />
+      <HomeMain name="Marcos" token={token} />
       <HomeWidget />
       <HomeCountdownSection />
       <HomeArticles />
