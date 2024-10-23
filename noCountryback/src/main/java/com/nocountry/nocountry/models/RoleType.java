a6b668efe3f42952e8cc48d920768b88a9e52276
Anderson Cusma <andersoncusma20@gmail.com>
2024-10-22 23:01:52 -0500

    sql import

--- noCountryback/src/main/java/com/nocountry/nocountry/models/RoleType.java
+++ noCountryback/src/main/java/com/nocountry/nocountry/models/RoleType.java
@@ -19,11 +19,13 @@ public class RoleType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "role_type_id")
     private UUID id;
 
+    @Column(name = "role_type_name")
     private String roleTypeName;
 
     @ManyToOne
-    @JoinColumn(name = "role_type_id", nullable = false,foreignKey = @ForeignKey(name = "FK_ROLES_TYPE_PROJECT_TYPE"))
+    @JoinColumn(name = "project_type_id", nullable = false,foreignKey = @ForeignKey(name = "FK_ROLES_TYPE_PROJECT_TYPE"))
     private ProjectType projectType;
 }
