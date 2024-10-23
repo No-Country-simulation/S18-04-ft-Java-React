c52ab2a59542683c976459088029b3927ae7ea6a
Diego Veras <verasdiegoca78@gmail.com>
2024-10-22 22:39:26 -0300

    Fix: add email confirmation.

--- noCountryback/src/main/java/com/nocountry/nocountry/controllers/ProfileController.java
+++ noCountryback/src/main/java/com/nocountry/nocountry/controllers/ProfileController.java
@@ -6,6 +6,7 @@ import com.nocountry.nocountry.dto.response.ProfileResponseDTO;
 import com.nocountry.nocountry.models.Profile;
 import com.nocountry.nocountry.security.oauth2.user.CurrentUser;
 import com.nocountry.nocountry.security.oauth2.user.UserPrincipal;
+import com.nocountry.nocountry.services.IEmailService;
 import com.nocountry.nocountry.services.IProfileService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/profiles")
 @RequiredArgsConstructor
 public class ProfileController {
+    private final IEmailService emailService;
     private final IProfileService service;
     private final ProfileMapper mapper;
 
@@ -37,7 +39,10 @@ public class ProfileController {
     @PostMapping
     public ResponseEntity<ProfileResponseDTO> save(@Valid @RequestBody ProfileRequestDTO dto, @CurrentUser UserPrincipal userPrincipal) {
         log.info("Current user Profile Save{}", userPrincipal.getName());
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.createProfile(dto, userPrincipal));
+        Profile profile = service.create(mapper.toProfile(dto));
+        emailService.registerConfirmation(profile.getUser().getEmail());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(mapper.toProfileResponseDTO(profile));
     }
 
     @PutMapping("/{id}")
