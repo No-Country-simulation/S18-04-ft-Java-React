5fe93f587ac28cb99da592fab13997df1884c24e
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-10-23 00:49:53 -0300

    fix: add try catch and node env validation

--- client/src/app/(authenticated)/home/page.jsx
+++ client/src/app/(authenticated)/home/page.jsx
@@ -4,12 +4,19 @@ import { HomeMain } from '@/components/sections/HomeMain/HomeMain';
 import { HomeWidget } from '@/components/sections/HomeWidget/HomeWidget';
 
 async function logPageView(token) {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
-  const apiUrl = `${baseUrl}/api/check`;
-  fetch(apiUrl, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json', TOKEN: token },
-  });
+  try {
+    const baseUrl =
+      process.env.NODE_ENV === 'production'
+        ? 'https://no-country.up.railway.app'
+        : 'http://localhost:3000';
+    const apiUrl = `${baseUrl}/api/check`;
+    fetch(apiUrl, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json', TOKEN: token },
+    });
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export default async function HomePage({ searchParams }) {
