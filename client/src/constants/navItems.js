aeb5801c5354334238c8406b148ec61af4a41576
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2024-10-27 03:06:29 -0300

    refactor: add redirect

--- client/src/constants/navItems.js
+++ client/src/constants/navItems.js
@@ -1,6 +1,6 @@
 export const navItems = [
   { href: '#', label: 'Perfil', icon: '/images/person.png' },
-  { href: '#', label: 'Mis simulaciones', icon: '/images/codesandbox.png' },
+  { href: '/simulations', label: 'Mis simulaciones', icon: '/images/codesandbox.png' },
   { href: '#', label: 'CV y Skills', icon: '/images/briefcase.png' },
   { href: '#', label: 'Salir', icon: '/images/power.png' },
 ];
