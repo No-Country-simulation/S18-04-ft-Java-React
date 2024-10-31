e313d868c3b5be4f3827fe2601429b629f3ef983
SoyIsabelMM <isabelmartinez19.30@gmail.com>
2024-10-31 14:45:04 -0300

    style: adjust arrow positioning

--- client/src/components/SimulationsPage/SimulationsPage.jsx
+++ client/src/components/SimulationsPage/SimulationsPage.jsx
@@ -6,19 +6,21 @@ import SimulationsContent from '../SimulationsContent/SimulationsContent';
 
 async function SimulationsPage() {
   return (
-    <div className="container mx-auto p-4">
-      <Link href="/home" className="absolute left-5 top-20 p-2" aria-label="Volver al perfil">
-        <Image
-          src="/images/arrowLeft.png"
-          className="gradient-custom stroke-[3]"
-          width={20}
-          height={20}
-          alt="Flecha izquierda"
-        />
-      </Link>
-      <Suspense fallback={<PinningLoader />}>
-        <SimulationsContent />
-      </Suspense>
+    <div className="container relative top-20 mx-auto p-4">
+      <div className="flex flex-col md:flex-row">
+        <Link href="/home" className="mb-4 inline-block md:mb-0" aria-label="Volver al perfil">
+          <Image
+            src="/images/arrowLeft.png"
+            className="gradient-custom stroke-[3]"
+            width={20}
+            height={20}
+            alt="Flecha izquierda"
+          />
+        </Link>
+        <Suspense fallback={<PinningLoader />}>
+          <SimulationsContent />
+        </Suspense>
+      </div>
     </div>
   );
 }
