6a1c774ba3757a42cfaf1b1454cab2108ab11d96
Catriel Escobar <125727012+Catriel-Escobar@users.noreply.github.com>
2024-10-31 17:44:45 -0300

    proyect id en cookie

--- noCountryback/src/main/java/com/nocountry/nocountry/services/IRoleTypeService.java
+++ noCountryback/src/main/java/com/nocountry/nocountry/services/IRoleTypeService.java
@@ -2,10 +2,11 @@ package com.nocountry.nocountry.services;
 
 import com.nocountry.nocountry.models.ProjectType;
 import com.nocountry.nocountry.models.RoleType;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface IRoleTypeService extends ICRUDService<RoleType, UUID> {
-    List<RoleType> findAllByProjectType(UUID projectTypeId);
+    List<RoleType> findAllByProjectType( UUID projectTypeId, HttpServletResponse resp );
 }
